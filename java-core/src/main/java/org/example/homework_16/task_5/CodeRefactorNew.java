e292e29e40214fab135eb184f3ad49235c86ead3
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-28 19:06:06 +0200

    Update CodeRefactorNew.java

--- java-core/src/main/java/org/example/homework_16/task_5/CodeRefactorNew.java
+++ java-core/src/main/java/org/example/homework_16/task_5/CodeRefactorNew.java
@@ -4,7 +4,7 @@ public class CodeRefactorNew {
     public static <Website, WebsiteType> void main(String[] args) {
         Website website = new Website("http://www.onliner.by");
 
-        String url = Website.getUrl();
+        String url = Website.getUrl(); // Website -> website
         WebsiteType type = website.getType();
 
         System.out.println(url);
