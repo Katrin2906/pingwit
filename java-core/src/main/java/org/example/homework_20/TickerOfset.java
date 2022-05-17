d970fff3118a5287e17deeed0ed6bba644aaca45
Alexander Fomin <RezviyBelorus@gmail.com>
2022-05-17 22:08:03 +0200

    Update TickerOfset.java

--- java-core/src/main/java/org/example/homework_20/TickerOfset.java
+++ java-core/src/main/java/org/example/homework_20/TickerOfset.java
@@ -16,10 +16,14 @@ public class TickerOfset {
         try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
             Statement statement = connection.createStatement();
 
+            /* 1. у тебя две разные по смыслу квери, не стоит их объеденять в одну операцию
+            2. Вариант с функцией в БД хороший, но лучше было вызвать LocalDateTime.now() и затем обновить записи таблицы
+            */
             ResultSet rs = statement.executeQuery("SELECT * FROM stocks s LIMIT 5 OFSET 4; UPDATE stocks SET update_date_time SELECT NOW();");
 
-            List<Stock> cars = new ArrayList<>();
+            List<Stock> cars = new ArrayList<>(); // List<Stock> стоит назвать stocks, а не cars
 
+            // таким образом ты получишь 5 записей с офсетом 4, а обновлять остальные кто будет?
             while (rs.next()) {
                 Stock stock = new Stock(
                         rs.getLong(1),
