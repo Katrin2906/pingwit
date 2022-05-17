854455eb992c683a7fee865bd8e2635aef739c8b
Alexander Fomin <RezviyBelorus@gmail.com>
2022-05-17 21:57:51 +0200

    Update Stock.java

--- java-core/src/main/java/org/example/homework_20/Stock.java
+++ java-core/src/main/java/org/example/homework_20/Stock.java
@@ -1,4 +1,5 @@
 package org.example.homework_20;
 
 public record Stock(Long id, String ticker, String companyName, Long profitYear, String location, Long updateDateTime) {
+  // для updateDateTime лучше подойдет LocalDatetime, а не Long
 }
