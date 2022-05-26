070ac0a7f644763b65a36275176d3145a9874133
Alexander Fomin <RezviyBelorus@gmail.com>
2022-05-26 15:44:08 +0200

    Update JavaSpringbootApplication.java

--- java-spring/src/main/java/homework_21/task_1/JavaSpringbootApplication.java
+++ java-spring/src/main/java/homework_21/task_1/JavaSpringbootApplication.java
@@ -2,6 +2,7 @@ package homework_21.task_1;
 
 import org.springframework.boot.SpringApplication;
 
+// этот класс не нужен. У приложения спринг бута будет 1 общий мейн класс с аннотацией @SpringBootApplication
 public class JavaSpringbootApplication {
 
     public static void main(String[] args) {
